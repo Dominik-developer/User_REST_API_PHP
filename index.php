cd2c40ed959fe6747c005b1e247f969cea7b0bbd
Dominik <163560221+Dominik-developer@users.noreply.github.com>
2025-02-26 23:31:30 +0100

    First code

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+
+    <!-- === CSS === -->
+    <link rel="stylesheet" href="src/main.css">
+
+    <!-- === JavaScript === -->
+    <script src="src/main.js"></script>
+
+    <!-- === Libraries === -->
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
